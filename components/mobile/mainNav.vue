fbd5419410ba286376d9e5e014b5c7ff7e1fc880
YeThura-424 <rathanpoudle.26@gmail.com>
2024-08-01 21:32:48 +0630

    update mainnavbar

--- components/mobile/mainNav.vue
+++ components/mobile/mainNav.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="nav-wrapper bg-[#ddd] w-full fixed bottom-0 rounded-t-lg p-3">
-    <ul class="nav-link-container flex justify-between items-center relative">
-      <li class="nav-link-item">
-        <img src="/images/home.svg" />
-      </li>
-      <li class="nav-link-item rounded-tr-xl">
-        <img src="/images/transaction.svg" />
-      </li>
-
-      <li class="nav-link-item">
-        <img src="/images/pie-chart.svg" />
-      </li>
-      <li class="nav-link-item">
-        <img src="/images/user.svg" />
-      </li>
-    </ul>
+  <div
+    class="nav-wrapper bg-[#fff] w-full fixed bottom-0 rounded-t-lg p-3 m-w-[56px]"
+  >
+    <div class="nav-link-container flex justify-between items-center relative">
+      <div class="flex gap-x-10">
+        <span class="nav-link-item">
+          <img src="/images/home.svg" />
+        </span>
+        <span class="nav-link-item rounded-tr-xl">
+          <img src="/images/transaction.svg" />
+        </span>
+      </div>
+      <div class="flex gap-x-10">
+        <span class="nav-link-item">
+          <img src="/images/pie-chart.svg" />
+        </span>
+        <span class="nav-link-item">
+          <img src="/images/user.svg" />
+        </span>
+      </div>
+    </div>
     <div
-      class="nav-link-item center-item absolute -top-2 left-1/2 -translate-x-1/2"
+      class="nav-link-item center-item absolute -top-1/2 -1/2 left-1/2 -translate-x-1/2"
     >
       <img src="/images/Add.svg" />
     </div>
@@ -24,7 +29,7 @@
 </template>
 <style scoped>
 .nav-link-item.center-item {
-  box-shadow: -1px -1px 0px 9px #ffffff;
+  box-shadow: -1px -1px 0px 9px #eee;
   background: #fff;
   border-radius: 25px;
 }
@@ -34,21 +39,21 @@
   position: absolute;
   width: 20px;
   height: 20px;
-  background: #dddddd;
+  background: #fff;
   border-top-right-radius: 20px;
-  top: 8px;
-  right: 69px;
-  box-shadow: 6px -7px 0px 6px #ffffff;
+  top: 28px;
+  right: 64px;
+  box-shadow: 5px -5px 0px 0px #eee;
 }
 .nav-link-item.center-item::after {
   content: "";
   position: absolute;
   width: 20px;
   height: 20px;
-  background: #dddddd;
+  background: #fff;
   border-top-left-radius: 20px;
-  top: 8px;
-  left: 65px;
-  box-shadow: -5px -6px 0px 3px #ffffff;
+  top: 28px;
+  left: 62px;
+  box-shadow: -2px -5px 0px 0px #eee;
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="bg-[#eee] h-screen">
     <slot />
     <MobileMainNav />
   </div>
