e18799c3eb488e0e88cd6fb342d51695c7abe788
YeThura-424 <rathanpoudle.26@gmail.com>
2024-08-02 21:28:04 +0630

    update recent transaction

--- components/core/RecentTransaction.vue
+++ components/core/RecentTransaction.vue
@@ -2,12 +2,7 @@
   <div class="w-full max-w-md px-2 py-16 sm:px-0">
     <TabGroup>
       <TabList class="flex space-x-1 rounded-xl p-1">
-        <Tab
-          v-for="category in Object.keys(categories)"
-          as="template"
-          :key="category"
-          v-slot="{ selected }"
-        >
+        <Tab as="template" v-slot="{ selected }">
           <button
             :class="[
               'px-3 rounded-2xl py-2 text-sm font-medium leading-5 ',
@@ -69,55 +64,4 @@
 <script setup>
 import { ref } from "vue";
 import { TabGroup, TabList, Tab, TabPanels, TabPanel } from "@headlessui/vue";
-
-const categories = ref({
-  Recent: [
-    {
-      id: 1,
-      title: "Does drinking coffee make you smarter?",
-      date: "5h ago",
-      commentCount: 5,
-      shareCount: 2,
-    },
-    {
-      id: 2,
-      title: "So you've bought coffee... now what?",
-      date: "2h ago",
-      commentCount: 3,
-      shareCount: 2,
-    },
-  ],
-  Popular: [
-    {
-      id: 1,
-      title: "Is tech making coffee better or worse?",
-      date: "Jan 7",
-      commentCount: 29,
-      shareCount: 16,
-    },
-    {
-      id: 2,
-      title: "The most innovative things happening in coffee",
-      date: "Mar 19",
-      commentCount: 24,
-      shareCount: 12,
-    },
-  ],
-  Trending: [
-    {
-      id: 1,
-      title: "Ask Me Anything: 10 answers to your questions about coffee",
-      date: "2d ago",
-      commentCount: 9,
-      shareCount: 5,
-    },
-    {
-      id: 2,
-      title: "The worst advice we've ever heard about coffee",
-      date: "4d ago",
-      commentCount: 1,
-      shareCount: 2,
-    },
-  ],
-});
 </script>
