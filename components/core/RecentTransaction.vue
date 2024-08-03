39faa62b516bcc56e02be4e3271a35fa21352a68
YeThura-424 <rathanpoudle.26@gmail.com>
2024-08-03 21:30:44 +0630

    recent transaction tab

--- components/core/RecentTransaction.vue
+++ components/core/RecentTransaction.vue
@@ -2,7 +2,12 @@
   <div class="w-full max-w-md px-2 py-16 sm:px-0">
     <TabGroup>
       <TabList class="flex space-x-1 rounded-xl p-1">
-        <Tab as="template" v-slot="{ selected }">
+        <Tab
+          v-for="(data, index) in title"
+          :key="index"
+          as="template"
+          v-slot="{ selected }"
+        >
           <button
             :class="[
               'px-3 rounded-2xl py-2 text-sm font-medium leading-5 ',
@@ -12,20 +17,24 @@
                 : 'text-[#91919F] hover:bg-white/[0.12] hover:text-white',
             ]"
           >
-            Day
+            {{ data }}
           </button>
         </Tab>
       </TabList>
 
       <TabPanels class="mt-2">
         <TabPanel
+          v-for="(data, index) in title"
+          :key="index"
           :class="[
             'rounded-xl bg-white p-3',
             'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
           ]"
         >
           <ul>
-            <li class="relative rounded-md p-3 hover:bg-gray-100"></li>
+            <li class="relative rounded-md p-3 hover:bg-gray-100">
+              {{ data }}
+            </li>
           </ul>
         </TabPanel>
       </TabPanels>
@@ -40,4 +49,12 @@ import {
   ShoppingBagIcon,
   ClipboardDocumentListIcon,
 } from "@heroicons/vue/20/solid";
+
+const title = ["Today", "Week", "Month", "Year"];
+const props = defineProps({
+  transactions: {
+    type: Object,
+    required: true,
+  },
+});
 </script>
