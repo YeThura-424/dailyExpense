1763716d78c030595acb013f111aaad18171d9c5
YeThura-424 <rathanpoudle.26@gmail.com>
2024-08-12 21:53:41 +0630

    update

--- components/mobile/pages/expense.vue
+++ components/mobile/pages/expense.vue
@@ -29,9 +29,9 @@
         id="wallet"
         class="text-xl px-4 py-2 rounded-lg h-14 w-full focus:outline-none border border-[#91919F]"
       >
-        <option value="val1">1</option>
-        <option value="val2">2</option>
-        <option value="val3">3</option>
+        <option value="val1" class="bg-[#eee] focus:bg-orange-100">1</option>
+        <option value="val2" class="bg-[#eee]">2</option>
+        <option value="val3" class="bg-[#eee]">3</option>
       </select>
     </div>
     <MobileMainNav />
