d8e14ae64f3fbf3c9ba718e2587a2434d30d7255
YeThura-424 <rathanpoudle.26@gmail.com>
2024-08-16 21:42:38 +0630

    update:layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@
     <Meta property="og:title" content="Expence Tracker" />
     <Meta property="og:type" content="website" />
   </Head>
-  <div class="bg-[#eee]">
+  <div class="bg-[#eee] h-full">
     <NuxtLoadingIndicator color="#801c1d" />
     <slot />
     <!-- <MobileMainNav /> -->
