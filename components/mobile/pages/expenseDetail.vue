0eedb36c6c544472432df664532446f67c823010
YeThura-424 <rathanpoudle.26@gmail.com>
2024-08-17 22:44:09 +0630

    update

--- components/mobile/pages/expenseDetail.vue
+++ components/mobile/pages/expenseDetail.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main-wrapper">
-    <div class="bg-[#FD3C4A] p-3">
-      <div class="header flex justify-between items-center">
+    <div class="bg-[#FD3C4A] p-3 rounded-b-[40px]">
+      <div class="main-header flex justify-between items-center">
         <div class="back_icon" @click="backAction">
           <Icon
             name="ion:arrow-back-circle-sharp"
@@ -17,6 +17,10 @@
             class="text-white text-3xl cursor-pointer"
           />
         </div>
+      </div>
+      <div class="main-body text-center text-white pt-20 pb-4">
+        <h1 class="amount font-extrabold text-5xl py-2">$120</h1>
+        <p class="text-base py-2">Sunday 4 June 2024 16:20</p>
       </div>
     </div>
   </div>
