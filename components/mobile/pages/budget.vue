9b4245d69ee50da3f6bf58aa49a9836105d807b9
YeThura-424 <rathanpoudle.26@gmail.com>
2024-08-18 22:04:33 +0630

    add:carousel in budget page

--- components/mobile/pages/budget.vue
+++ components/mobile/pages/budget.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
-    <div class="budget-month-carousel"></div>
+    <div class="budget-month-carousel bg-[#7F3DFF] text-white px-3 py-4">
+      <Carousel>
+        <Slide v-for="month in months" :key="slide">
+          <div class="carousel__item">{{ month }}</div>
+        </Slide>
+
+        <template #addons>
+          <Navigation class="budget-carousel" />
+        </template>
+      </Carousel>
+    </div>
   </div>
 </template>
+<script setup>
+const months = [
+  "January",
+  "Febuary",
+  "March",
+  "Apirl",
+  "May",
+  "June",
+  "July",
+  "Augest",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+</script>
