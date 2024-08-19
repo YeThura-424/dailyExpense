08155f8fbcf538d5fb0b0109d63877e19b70b9e7
YeThura-424 <rathanpoudle.26@gmail.com>
2024-08-19 21:23:05 +0630

    budget: empty and listing

---
+++ components/mobile/Budget/EmptyState.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="empty-budget text-[#91919F] min-h-[400px] max-w-[450px] flex items-center justify-center"
+  >
+    <div class="text-center">
+      <p class="text-base font-medium">You don't have a budget</p>
+      <p class="text-base font-medium">Let's make one so you in control</p>
+    </div>
+  </div>
+</template>
--- components/mobile/pages/budget.vue
+++ components/mobile/pages/budget.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="min-h-screen">
+  <div class="bg-[#7F3DFF]">
     <div class="budget-month-carousel bg-[#7F3DFF] text-white px-3 pt-8 pb-12">
       <Carousel>
         <Slide v-for="month in months" :key="slide">
-          <div class="carousel__item">{{ month }}</div>
+          <div class="carousel__item text-lg">{{ month }}</div>
         </Slide>
 
         <template #addons>
@@ -11,7 +11,23 @@
         </template>
       </Carousel>
     </div>
-    <div class="budget-list"></div>
+    <div class="budget-list rounded-t-[40px] bg-[#eee] relative">
+      <MobileBudgetEmptyState v-if="true" />
+      <div
+        class="budget-create absolute bottom-10 w-[95%] left-1/2 -translate-x-1/2"
+      >
+        <button
+          type="button"
+          class="w-full flex items-center justify-center gap-x-2 rounded-md border border-transparent bg-[#7F3DFF] text-white px-4 py-1.5 text-lg font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 cursor-pointer"
+        >
+          <Icon
+            name="ion:ios-create"
+            class="text-white text-2xl cursor-pointer"
+          />
+          Create a budget
+        </button>
+      </div>
+    </div>
     <MobileMainNav />
   </div>
 </template>
@@ -36,5 +52,9 @@ const months = [
 button.carousel__next.budget-carousel,
 button.carousel__prev.budget-carousel {
   color: white !important;
+}
+
+.budget-list {
+  height: calc(100vh - 164px);
 }
 </style>
