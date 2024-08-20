7ee61f34c5092f1123e24d8a2df7d492c51ecdc4
YeThura-424 <rathanpoudle.26@gmail.com>
2024-08-20 21:48:08 +0630

    budget create

---
+++ components/mobile/pages/budgetCreate.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>budget create</div>
+</template>
---
+++ pages/budget/create.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div>
+    <LazyDesktop v-if="$device.isDesktopOrTablet" />
+    <LazyMobilePagesBudgetCreate v-else />
+  </div>
+</template>
