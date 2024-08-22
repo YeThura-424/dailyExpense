478575054a00fa4a6f0942e7b5ce457e512239a2
YeThura-424 <rathanpoudle.26@gmail.com>
2024-08-22 21:15:06 +0630

    profile page

---
+++ components/mobile/pages/profile.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="px-4 pt-5 pb-[78px]"></div>
+
+    <MobileMainNav />
+  </div>
+</template>
+
+<script setup></script>
---
+++ pages/profile.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div>
+    <LazyDesktop v-if="$device.isDesktopOrTablet" />
+    <LazyMobilePagesProfile v-else />
+  </div>
+</template>
