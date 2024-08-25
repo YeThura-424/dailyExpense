8079fb63e4569eed3cc049700f4858b3658e1006
YeThura-424 <rathanpoudle.26@gmail.com>
2024-08-25 22:01:16 +0630

    update

--- components/core/FileUpload.vue
+++ components/core/FileUpload.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="upload-wrapper border border-[#91919F]">
+  <div
+    :class="[
+      'upload-wrapper border border-[#91919F] h-40 rounded-xl p-3 flex',
+      previewUrl ? 'justify-start gap-3' : 'justify-center items-center',
+    ]"
+  >
+    <div class="preview-wrapper" v-if="previewUrl">
+      <div
+        class="preview relative w-24 h-24 border border-dashed border-[#91919F] rounded-xl"
+      >
+        <img :src="previewUrl" alt="" class="w-24 h-24 rounded-xl p-1" />
+        <Icon
+          name="ion:ios-close-circle"
+          class="text-[#666666] absolute top-0 right-0 cursor-pointer"
+          size="24"
+        />
+      </div>
+    </div>
     <div
-      class="attachment_inpu border border-dashed border-[#91919F] flex flex-col items-center justify-center py-3 rounded-xl"
+      class="attachment_input w-24 h-24 border border-dashed border-[#91919F] flex flex-col items-center justify-center rounded-xl"
     >
       <Icon
         name="ion:ios-attach"
@@ -15,11 +32,6 @@
         class="absolute opacity-0"
         @change="handleUpload"
       />
-    </div>
-    <div class="preview-wrapper">
-      <div class="preview">
-        <img :src="previewUrl" alt="" />
-      </div>
     </div>
   </div>
 </template>
