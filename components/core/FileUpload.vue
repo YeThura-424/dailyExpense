76a940de35e671abe02f5fe227ba8213095e4f01
YeThura-424 <rathanpoudle.26@gmail.com>
2024-08-25 13:01:49 +0630

    upload component

--- components/core/FileUpload.vue
+++ components/core/FileUpload.vue
@@ -1,14 +1,26 @@
 <template>
-  <div
-    class="attachment_inpu border border-dashed border-[#91919F] flex items-center justify-center py-3 rounded-xl"
-  >
-    <Icon
-      name="ion:ios-attach"
-      size="30"
-      class="text-[#91919F] -rotate-[135deg]"
-    />
-    <span class="text-[#91919F] text-lg">Add attachment</span>
-    <input id="file-upload" type="file" class=" absolute opacity-0"
+  <div class="upload-wrapper border border-[#91919F]">
+    <div
+      class="attachment_inpu border border-dashed border-[#91919F] flex flex-col items-center justify-center py-3 rounded-xl"
+    >
+      <Icon
+        name="ion:ios-attach"
+        size="30"
+        class="text-[#91919F] -rotate-[135deg]"
+      />
+      <span class="text-[#91919F] text-lg">Add attachment</span>
+      <input
+        id="file-upload"
+        type="file"
+        class="absolute opacity-0"
+        @change="handleUpload"
+      />
+    </div>
+    <div class="preview-wrapper">
+      <div class="preview">
+        <img :src="previewUrl" alt="" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,7 +31,13 @@ const props = defineProps({
     default: true,
   },
 });
-const files = ref(null);
+const uploadFiles = ref(null);
+const previewUrl = ref(null);
+const handleUpload = (e) => {
+  console.log("uploaded", e.target.files);
+  uploadFiles.value = e.target.files;
+  previewUrl.value = URL.createObjectURL(uploadFiles.value[0]);
+};
 </script>
 
 <style scoped>
