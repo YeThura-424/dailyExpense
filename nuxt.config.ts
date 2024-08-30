39fd6309dbd5a5ae01f2a1dece6d8f264c96b10e
YeThura-424 <rathanpoudle.26@gmail.com>
2024-08-30 22:03:47 +0630

    add:pinia module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxtjs/device", "@nuxt/icon", "vue3-carousel-nuxt"],
+  modules: [
+    "@nuxtjs/device",
+    "@nuxt/icon",
+    "vue3-carousel-nuxt",
+    "@pinia/nuxt",
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
