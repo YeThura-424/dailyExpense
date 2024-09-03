3a21ef225df411f9619583b89157ca14cf482142
YeThura-424 <rathanpoudle.26@gmail.com>
2024-09-03 21:11:44 +0630

    login

--- components/mobile/pages/auth/login.vue
+++ components/mobile/pages/auth/login.vue
@@ -3,26 +3,33 @@
     <MobilePageHeader title="Login" text-color="text-black" />
     <div class="login-form-wrapper pt-16">
       <div class="username py-3">
-        <CoreInputBox placeholder="Email" type="email" />
+        <CoreInputBox
+          placeholder="Email"
+          type="email"
+          v-model="form.username"
+        />
       </div>
 
       <div class="password py-3">
-        <CoreInputBox placeholder="password" type="password" />
+        <CoreInputBox
+          placeholder="password"
+          type="password"
+          v-model="form.password"
+        />
       </div>
 
-      <div class="budget-create py-3">
-        <nuxt-link to="/account/add">
-          <button
-            type="button"
-            class="w-full flex items-center justify-center gap-x-2 rounded-md border border-transparent bg-[#7F3DFF] text-white px-4 py-1.5 text-lg font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 cursor-pointer"
-          >
-            <Icon
-              name="ion:ios-log-in"
-              class="text-white text-2xl cursor-pointer"
-            />
-            Login
-          </button>
-        </nuxt-link>
+      <div class="login py-3">
+        <button
+          @click="login"
+          type="button"
+          class="w-full flex items-center justify-center gap-x-2 rounded-md border border-transparent bg-[#7F3DFF] text-white px-4 py-1.5 text-lg font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 cursor-pointer"
+        >
+          <Icon
+            name="ion:ios-log-in"
+            class="text-white text-2xl cursor-pointer"
+          />
+          Login
+        </button>
       </div>
 
       <div class="forget-password text-center py-3">
@@ -38,3 +45,17 @@
     </div>
   </div>
 </template>
+
+<script setup>
+const user = useAuth();
+
+const form = reactive({
+  username: "",
+  password: "",
+});
+
+const login = () => {
+  user.login(form);
+  navigateTo("/");
+};
+</script>
