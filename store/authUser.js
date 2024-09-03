122182c3b4749fdc49ebcc7567c2e0ae89f319b5
YeThura-424 <rathanpoudle.26@gmail.com>
2024-09-03 21:11:58 +0630

    login method in auth store

--- store/authUser.js
+++ store/authUser.js
@@ -1,12 +1,26 @@
 export const useauthUserStore = defineStore("authUser", {
   state: () => ({
     user: {},
-    token: "1234565",
+    token: "",
   }),
   getters: {},
   actions: {
-    setUser() {
-      this.user = "Logged in user";
+    login(req) {
+      let userinfo = {
+        username: "test112",
+        password: "secret",
+      };
+      console.log(req);
+      if (
+        req.username == userinfo.username &&
+        req.password == userinfo.password
+      ) {
+        this.user = "Logged in user";
+        this.token = req.password;
+      }
+
+      console.log("token", this.token);
     },
+    setUser() {},
   },
 });
