40048b5687a946286537a5a90fff280ee888b6e8
YeThura-424 <rathanpoudle.26@gmail.com>
2024-09-17 20:57:31 +0630

    update:server api

--- server/api/[...].js
+++ server/api/[...].js
@@ -6,15 +6,33 @@ export default defineEventHandler(async (event) => {
     body = await readBody(event);
   }
 
+  let query = "";
+  let split = "";
+
+  if (event.req.url.includes("?")) {
+    split = event.req.url.split("?");
+    query = split[1];
+  }
   const token = getCookie(event, "token");
-  const { data } = await $fetch(`http://localhost:8000/api/${paramPath}`, {
-    method: method,
-    headers: {
-      Accept: "application/json",
-      Authorization: `Bearer ${token}`,
-      "x-api-token": "4fGh9Kj7Lm1Nq2RzXw8T",
-    },
-    body: body,
-  });
-  return data;
+  const response = await $fetch(
+    `http://localhost:8000/api/${paramPath}?${query}`,
+    {
+      method: method,
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+        "x-api-token": "4fGh9Kj7Lm1Nq2RzXw8T",
+      },
+      body: body,
+    }
+  );
+  let data = null;
+
+  if (response.hasOwnProperty("json")) {
+    data = await response.json();
+  } else {
+    data = response;
+  }
+
+  return { data };
 });
