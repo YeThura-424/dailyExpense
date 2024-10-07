a8ce4b4a69ea64dcb109cc36e9089013f5a3bb46
YeThura-424 <rathanpoudle.26@gmail.com>
2024-10-07 21:54:40 +0630

    add:datepicker component

---
+++ components/core/DatePicker.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+        <VueDatePicker v-model="date" />
+  </div>
+</template>
+
+<script setup>
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+const date = ref(null);
+</script>
