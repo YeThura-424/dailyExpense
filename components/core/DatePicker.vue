da0a5b7b7b71e76085cca611550f56c7d806904a
YeThura-424 <rathanpoudle.26@gmail.com>
2024-10-08 20:37:21 +0630

    update:datepicker ui

--- components/core/DatePicker.vue
+++ components/core/DatePicker.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="date-picker-wrapper">
         <VueDatePicker v-model="date" />
   </div>
 </template>
@@ -10,3 +10,19 @@ import '@vuepic/vue-datepicker/dist/main.css'
 
 const date = ref(null);
 </script>
+
+<style>
+.dp__input{
+  height: 56px;
+  padding: 0;
+  margin: 0;
+  border: 1px solid #91919f;
+  border-radius: 0.75rem;
+}
+
+.dp__input_icons{
+  font-size: 34px;
+  width: 25px;
+  height: 25px;
+}
+</style>
