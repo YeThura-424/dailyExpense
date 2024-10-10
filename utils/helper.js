71a70f837adc3b4db885cc3dc328af360a1b5ba5
YeThura-424 <rathanpoudle.26@gmail.com>
2024-10-10 21:15:28 +0630

    create getpreviousmonth method in helper

--- utils/helper.js
+++ utils/helper.js
@@ -4,3 +4,18 @@ export const toTitleCase = (str) => {
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) // Capitalize first letter of each word
     .join(" "); // Join words with spaces
 };
+
+export const getPreviousMonth = () => {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth();
+  const previousMonths = [];
+  for (let i = 0; i < currentMonth; i++) {
+    previousMonths.push({
+      id: i + 1,
+      name: new Date(currentDate.getFullYear(), i).toLocaleString('default', { month: 'long' }),
+      value: i
+    })
+  }
+
+  return previousMonths;
+}
