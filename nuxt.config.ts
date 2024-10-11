ac43abcabed00707d3732e8e274a1d71600d2f92
YeThura-424 <rathanpoudle.26@gmail.com>
2024-10-11 22:07:51 +0630

    update nust config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,19 +25,4 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  // nitro: {
-  //   routeRules: {
-  //     "/api/**": {
-  //       headers: {
-  //         "Access-Control-Allow-Origin": "*",
-  //         "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-  //         "Access-Control-Allow-Headers":
-  //           "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-  //       },
-  //     },
-  //   },
-  // },
-  // routeRules: {
-  //   "/api/**": { proxy: { to: "http://localhost:8000/**" } },
-  // },
 });
