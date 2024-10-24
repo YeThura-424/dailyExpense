292fa2f42873e7963a0fc621a84c1e271ad955ba
YeThura-424 <rathanpoudle.26@gmail.com>
2024-10-24 21:56:31 +0630

    budget add

--- components/mobile/pages/budgetCreate.vue
+++ components/mobile/pages/budgetCreate.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="bg-[#7F3DFF] flex flex-col justify-between h-screen">
-    <div
-      class="budget_main_content px-6 py-4 flex flex-col justify-between h-full"
-    >
+    <div class="budget_main_content px-6 py-4 flex flex-col justify-between h-full">
       <div class="budget_header pt-3">
         <MobilePageHeader title="Create Budget" icon-color="text-white" @back="backAction" />
       </div>
@@ -10,23 +8,15 @@
         <span class="text-lg">How much do you want to spend ?</span>
         <div class="flex items-center">
           <span class="font-extrabold text-5xl">$</span>
-          <input
-            v-model="form.amount"
-            type="text"
-            class="bg-transparent focus:outline-none font-extrabold text-5xl px-2 w-2/3"
-          />
+          <input v-model="form.amount" type="text"
+            class="bg-transparent focus:outline-none font-extrabold text-5xl px-2 w-2/3" />
         </div>
       </div>
     </div>
     <div class="budget_form bg-white rounded-t-[40px] shadow px-6 py-4">
       <div class="category_select py-3">
-        <CoreSelectBox
-          :options="categories"
-          option-key="id"
-          name="Category"
-          placeholder="Select Category"
-          v-model="form.category"
-        />
+        <CoreSelectBox :options="categories" option-key="id" name="Category" placeholder="Select Category"
+          v-model="form.category" />
       </div>
 
       <div class="repeat-transaction flex justify-between items-center py-3">
@@ -38,28 +28,19 @@
         </div>
         <div class="toggle">
           <Switch v-model="form.alert" as="template" v-slot="{ checked }">
-            <button
-              class="relative inline-flex h-6 w-11 items-center rounded-full"
-              :class="checked ? 'bg-[#7F3DFF]' : 'bg-gray-200'"
-            >
+            <button class="relative inline-flex h-6 w-11 items-center rounded-full"
+              :class="checked ? 'bg-[#7F3DFF]' : 'bg-gray-200'">
               <span class="sr-only">Enable notifications</span>
-              <span
-                :class="checked ? 'translate-x-6' : 'translate-x-1'"
-                class="inline-block h-4 w-4 transform rounded-full bg-white transition"
-              />
+              <span :class="checked ? 'translate-x-6' : 'translate-x-1'"
+                class="inline-block h-4 w-4 transform rounded-full bg-white transition" />
             </button>
           </Switch>
         </div>
       </div>
       <div class="save-button flex justify-end gap-x-5 py-3">
-        <button
-          type="button"
-          class="w-full flex items-center gap-x-2 rounded-md border border-transparent bg-[#7F3DFF] text-white px-4 py-1.5 text-lg font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 cursor-pointer"
-        >
-          <Icon
-            name="ion:save-outline"
-            class="text-white text-2xl cursor-pointer"
-          />
+        <button type="button"
+          class="w-full flex items-center gap-x-2 rounded-md border border-transparent bg-[#7F3DFF] text-white px-4 py-1.5 text-lg font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 cursor-pointer">
+          <Icon name="ion:save-outline" class="text-white text-2xl cursor-pointer" />
           Continue
         </button>
       </div>
@@ -70,15 +51,11 @@
 <script setup>
 import { Switch } from "@headlessui/vue";
 
-const router = useRouter();
 const form = reactive({
   amount: 0,
   category: "",
   alert: false,
 });
-const setDescription = (val) => {
-  form.description = val;
-};
 
 const backAction = () => {
   navigateTo('/budget');
@@ -86,7 +63,7 @@ const backAction = () => {
 
 const categories = ref([]);
 
-const fetchCategory = async() => {
+const fetchCategory = async () => {
   try {
     await useFetch("/api/category", {
       method: "GET",
