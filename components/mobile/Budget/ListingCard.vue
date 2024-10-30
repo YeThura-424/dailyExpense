8c8470e7664d7cd121d2a436fa12d4fa5abf46fd
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2024-10-30 22:15:41 +0630

    update

--- components/mobile/Budget/ListingCard.vue
+++ components/mobile/Budget/ListingCard.vue
@@ -1,42 +1,44 @@
 <template>
   <div>
     <div
-    class="listing-card-wrapper p-4 bg-[#ffffff] rounded-2xl mb-3"
-    v-for="budget in budgets"
-    :key="budget"
-  >
-    <div class="card-header flex justify-between items-center">
-      <div class="header-type py-2">
-        <button
-          class="flex items-center text-lg gap-x-1 bg-[#F1F1FA] rounded-full px-3 py-1.5"
+      class="listing-card-wrapper p-4 bg-[#ffffff] rounded-2xl mb-3"
+      v-for="budget in budgets"
+      :key="budget"
+    >
+      <div class="card-header flex justify-between items-center">
+        <div class="header-type py-2">
+          <button
+            class="flex items-center text-lg gap-x-1 bg-[#F1F1FA] rounded-full px-3 py-1.5"
+          >
+            <Icon name="ion:ios-radio-button-on" :class="alertTheme(budget)" />
+            {{ budget.category.name }}
+          </button>
+        </div>
+        <div class="header-alert">
+          <Icon
+            name="ion:information-circled"
+            :class="['text-3xl', alertTheme(budget)]"
+          />
+        </div>
+      </div>
+      <div class="card-info">
+        <h1 class="text-xl font-bold pb-2">
+          Remaining ${{ remaining(budget) }}
+        </h1>
+        <div class="budget-bar-wrapper rounded-lg w-full bg-slate-300 flex">
+          <div
+            :class="['budget-bar p-2 rounded-lg', alertBg(budget)]"
+            :style="{ width: spentPercent(budget) }"
+          ></div>
+        </div>
+        <p class="text-[#6f6f74] text-lg">
+          $ {{ budget.spend_amount ?? 0 }} of $ {{ budget.total }}
+        </p>
+        <span v-if="isExceed(budget)" :class="['text-xl', alertTheme(budget)]"
+          >You've exceed the limit!</span
         >
-          <Icon name="ion:ios-radio-button-on" :class="alertTheme(budget)" />
-          {{ budget.category.name }}
-        </button>
-      </div>
-      <div class="header-alert">
-        <Icon
-          name="ion:information-circled"
-          :class="['text-3xl', alertTheme(budget)]"
-        />
       </div>
     </div>
-    <div class="card-info">
-      <h1 class="text-xl font-bold pb-2">Remaining ${{ remaining(budget) }}</h1>
-      <div class="budget-bar-wrapper rounded-lg w-full bg-slate-300 flex">
-        <div
-          :class="['budget-bar p-2 rounded-lg', alertBg(budget)]"
-          :style="{ width: spentPercent(budget) }"
-        ></div>
-      </div>
-      <p class="text-[#6f6f74] text-lg">
-        $ {{ budget.spend_amount??0 }} of $ {{ budget.total }}
-      </p>
-      <span v-if="isExceed(budget)" :class="['text-xl', alertTheme(budget)]"
-        >You've exceed the limit!</span
-      >
-    </div>
-  </div>
   </div>
 </template>
 <script setup>
@@ -48,15 +50,15 @@ const props = defineProps({
 });
 
 const remaining = (budget) => {
-  return budget.total??0 - budget.spend_amount??0;
+  return budget.total ?? 0 - budget.spend_amount ?? 0;
 };
 
 const isExceed = (budget) => {
-  return budget.spend_amount??0 >= budget.total;
+  return budget.spend_amount ?? 0 >= budget.total;
 };
 
 const alertTheme = (budget) => {
-  const spentPercent = (budget.spend_amount??0 / budget.total) * 100;
+  const spentPercent = (budget.spend_amount ?? 0 / budget.total) * 100;
 
   if (spentPercent < 51) return "text-blue-500";
   if (spentPercent > 100) return "text-red-500";
@@ -65,7 +67,7 @@ const alertTheme = (budget) => {
 };
 
 const alertBg = (budget) => {
-  const percent = (budget.spend_amount??0 / budget.total) * 100;
+  const percent = (budget.spend_amount ?? 0 / budget.total) * 100;
 
   if (percent < 51) return "bg-blue-500";
   if (percent > 100) return "bg-red-500";
@@ -74,7 +76,7 @@ const alertBg = (budget) => {
 };
 
 const spentPercent = (budget) => {
-  const percent = Math.floor((budget.spend_amount??0 / budget.total) * 100);
+  const percent = Math.floor((budget.spend_amount ?? 0 / budget.total) * 100);
 
   return `${percent}%`;
 };
