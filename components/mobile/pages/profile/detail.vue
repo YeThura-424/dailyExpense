e00599435ee1314792ab5f7be54cad22d983abfd
YeThura-424 <rathanpoudle.26@gmail.com>
2024-10-31 21:40:51 +0630

    update:core inputbox and profile detail

--- components/mobile/pages/profile/detail.vue
+++ components/mobile/pages/profile/detail.vue
@@ -12,16 +12,33 @@
         </div>
       </div>
       <div class="profile-tab-content">
-        <div v-if="activeTab == 'info'" class="profile-info flex justify-center py-4">
+        <div v-if="activeTab == 'info'" class="profile-info flex flex-col justify-center items-center gap-y-4 py-4">
           <div
             class="user-prifile relative w-[200px] h-[200px] rounded-full border border-[#7f3dff] flex justify-center items-center">
-            <img :src="user?.image ? user?.image : '/images/userprofile.png'" alt=""
+            <img v-if="userProfilePreview" :src="userProfilePreview" />
+            <img v-else :src="user?.image ? user?.image : '/images/userprofile.png'" alt=""
               class="w-[190px] h-[190px] border border-[#ccbaf0] rounded-full" />
-            <div class="upload-icon absolute bottom-9 right-1 bg-[#eee] h-6 cursor-pointer">
-              <input id="upload-prifile" type="file" class="hidden" />
-              <label for="upload-profile">
+            <div class="upload-icon absolute bottom-9 right-1 bg-[#eee] h-6">
+              <div for="upload-profile relative w-6 h-6">
                 <Icon name="ion:camera-outline" size="24" class="text-[#7f3dff]" />
-              </label>
+                <input id="upload-prifile" type="file" @change="handleUpload"
+                  class="opacity-0 w-full h-full absolute right-0 cursor-pointer" />
+              </div>
+            </div>
+          </div>
+          <div class="user-profile-info w-full py-4 px-6">
+            <div class="username py-2">
+              <label class="text-lg text-[#3f4142] font-medium" for="username">Name</label>
+              <CoreInputBox v-model="userInfo.name" type="text" />
+            </div>
+            <div class="email py-2">
+              <label class="text-lg text-[#3f4142] font-medium" for="username">Email</label>
+              <CoreInputBox v-model="userInfo.email" type="email" disabled />
+            </div>
+            <div class="currency py-2">
+              <label class="text-lg text-[#3f4142] font-medium" for="currency">Currency</label>
+              <CoreSelectBox v-model="userInfo.currency" placeholder="Select Default Currency" :options="currency"
+                show-icon />
             </div>
           </div>
         </div>
@@ -34,22 +51,38 @@
 </template>
 
 <script setup>
-
-const route = useRoute();
 const user = useCookie('user');
+const userInfo = reactive({
+  name: user.value.name,
+  profile: user.value.image,
+  email: user.value.email,
+  currency: user.value.currency,
+})
 
+const userProfilePreview = ref(null);
 const tabs = [
   { name: 'Info', key: 'info' },
   { name: 'Password', key: 'password' }
 ]
+
+const currency = ref([
+  { name: 'Ks', value: 'ks', icon: '/images/ks.png' },
+  { name: 'USD', value: 'usd', icon: '/images/dollar.png' },
+  { name: 'Baht', value: 'bhat', icon: '/images/baht.png' },
+])
 
 const activeTab = ref(tabs[0].key);
 
 const changeTab = (tab) => {
   activeTab.value = tab;
 }
+
 const backAction = () => {
   navigateTo('/profile');
 }
 
+const handleUpload = (e) => {
+  userInfo.profile = e.target.files;
+  userProfilePreview.value = URL.createObjectURL(e.target.files[0])
+};
 </script>
