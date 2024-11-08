90672969af5bf0bdc94d8d151202d4a5098ee4df
YeThura-424 <rathanpoudle.26@gmail.com>
2024-11-08 22:28:07 +0630

    update:income expend loading com

--- components/mobile/Loading/IncomeExpenseCard.vue
+++ components/mobile/Loading/IncomeExpenseCard.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="flex justify-between items-center bg-[#FCFCFC] p-4 rounded-lg mt-2">
-    <div class="transaction_type flex gap-x-4">
+  <div class="flex justify-between items-center gap-x-6 bg-[#FCFCFC] p-4 rounded-lg mt-2">
+    <div class="transaction_type flex w-full gap-x-4">
       <div :class="[
         'transaction_list_icon w-14 h-14 hide-text rounded-md',
       ]">
 
       </div>
-      <div class="transaction_list_desctiption">
-        <h1 class="h-7 hide-text rounded">
+      <div class="transaction_list_desctiption w-full">
+        <h1 class="h-7 hide-text rounded mb-2">
 
         </h1>
-        <span class="h-5 hide-text rounded"></span>
+        <p class="h-5 hide-text rounded"></p>
       </div>
     </div>
-    <div class="transaction_list_amount text-right">
-      <h1 class="h-7 hide-text rounded">
+    <div class="transaction_list_amount w-full text-right">
+      <h1 class="h-7 hide-text rounded mb-2">
 
       </h1>
-      <span class="h-5 hide-text rounded"></span>
+      <p class="h-5 hide-text rounded"></p>
     </div>
   </div>
 </template>
