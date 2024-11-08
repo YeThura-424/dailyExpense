f3d3d0b55d04a809526bb1360cefaa62eee9ebad
YeThura-424 <rathanpoudle.26@gmail.com>
2024-11-08 22:18:58 +0630

    income expense loading card

---
+++ components/mobile/Loading/IncomeExpenseCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-between items-center bg-[#FCFCFC] p-4 rounded-lg mt-2">
+    <div class="transaction_type flex gap-x-4">
+      <div :class="[
+        'transaction_list_icon w-14 h-14 hide-text rounded-md',
+      ]">
+
+      </div>
+      <div class="transaction_list_desctiption">
+        <h1 class="h-7 hide-text rounded">
+
+        </h1>
+        <span class="h-5 hide-text rounded"></span>
+      </div>
+    </div>
+    <div class="transaction_list_amount text-right">
+      <h1 class="h-7 hide-text rounded">
+
+      </h1>
+      <span class="h-5 hide-text rounded"></span>
+    </div>
+  </div>
+</template>
