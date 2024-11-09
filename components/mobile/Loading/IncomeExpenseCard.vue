eb5bc531397090aa1f4d057f1c60fc500767e5b2
YeThura-424 <rathanpoudle.26@gmail.com>
2024-11-09 21:08:39 +0630

    recent transaction loading, component

--- components/mobile/Loading/IncomeExpenseCard.vue
+++ components/mobile/Loading/IncomeExpenseCard.vue
@@ -1,19 +1,20 @@
 <template>
-  <div class="flex justify-between items-center gap-x-6 bg-[#FCFCFC] p-4 rounded-lg mt-2">
-    <div class="transaction_type flex w-full gap-x-4">
+  <div v-for="data in cards" :key="data"
+    class="flex justify-between items-center gap-x-6 bg-[#FCFCFC] p-4 rounded-lg mt-2">
+    <div class="transaction_type w-3/4 flex gap-x-4">
       <div :class="[
-        'transaction_list_icon w-14 h-14 hide-text rounded-md',
+        'transaction_list_icon w-1/3 h-14 hide-text rounded-md',
       ]">
 
       </div>
-      <div class="transaction_list_desctiption w-full">
+      <div class="transaction_list_desctiption w-3/4">
         <h1 class="h-7 hide-text rounded mb-2">
 
         </h1>
         <p class="h-5 hide-text rounded"></p>
       </div>
     </div>
-    <div class="transaction_list_amount w-full text-right">
+    <div class="transaction_list_amount w-1/4 text-right">
       <h1 class="h-7 hide-text rounded mb-2">
 
       </h1>
@@ -21,3 +22,12 @@
     </div>
   </div>
 </template>
+
+<script setup>
+const props = defineProps({
+  cards: {
+    type: Number,
+    default: 3
+  }
+})
+</script>
