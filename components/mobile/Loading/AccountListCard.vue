a9a7c7cf68747949a84e8a8d178adc03414ae089
YeThura-424 <rathanpoudle.26@gmail.com>
2024-11-11 22:49:50 +0630

    create:account loading card, add:loading state in account page

---
+++ components/mobile/Loading/AccountListCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="account-section flex items-center gap-x-2 justify-between border-b border-[#eee] px-4 py-4">
+    <div class="flex items-center gap-x-3 w-2/3">
+      <div class="account-logo w-1/4 h-14 rounded-2xl hide-text">
+
+      </div>
+      <div class="w-3/4">
+        <h1 class=" h-7 w-full mb-2 rounded-md hide-text">
+
+        </h1>
+        <p class="capitalize h-5 mt-1 rounded hide-text"></p>
+      </div>
+    </div>
+    <div class="amount w-1/3">
+      <p class="h-7 rounded-md hide-text"></p>
+    </div>
+  </div>
+</template>
--- components/mobile/pages/account.vue
+++ components/mobile/pages/account.vue
@@ -10,31 +10,37 @@
           ${{ walletLists.total_amount }}
         </p>
       </div>
-      <div v-if="walletLists?.user_wallet?.length == 0" class="empty_account_state py-12">
-        <div class="text-center">
-          <!-- <IconAccount /> -->
-          <p class="text-base font-medium">You don't any account</p>
-          <p class="text-base font-medium">Let's make one so you in control</p>
+      <div v-if="walletLoading">
+        <div v-for="data in 5" :key="data">
+          <MobileLoadingAccountListCard />
         </div>
       </div>
-
-      <div v-else class="account-modules-wrapper bg-white overflow-auto">
-        <!-- wallet account  -->
-        <div v-for="wallet in walletLists.user_wallet" :key="wallet"
-          class="account-section flex items-center justify-between border-b border-[#eee] px-4 py-4">
-          <div class="flex items-center gap-x-3">
-            <div class="account-logo w-14 h-14 bg-[#EEE5FF] rounded-2xl flex justify-center items-center">
-              <Icon name="ion:md-wallet" class="text-4xl text-[#7F3DFF]" />
-            </div>
-            <div>
-              <h1 class="text-[#292B2D] text-xl font-medium">
-                {{ wallet.name }}
-              </h1>
-              <p class="capitalize">{{ wallet.wallet_type.name }}</p>
-            </div>
+      <div v-else>
+        <div v-if="walletLists?.user_wallet?.length == 0" class="empty_account_state py-12">
+          <div class="text-center">
+            <!-- <IconAccount /> -->
+            <p class="text-base font-medium">You don't any account</p>
+            <p class="text-base font-medium">Let's make one so you in control</p>
           </div>
-          <div class="amount">
-            <p class="text-xl text-[#212325]">${{ wallet.amount }}</p>
+        </div>
+        <div v-else class="account-modules-wrapper bg-white overflow-auto">
+          <!-- wallet account  -->
+          <div v-for="wallet in walletLists.user_wallet" :key="wallet"
+            class="account-section flex items-center justify-between border-b border-[#eee] px-4 py-4">
+            <div class="flex items-center gap-x-3">
+              <div class="account-logo w-14 h-14 bg-[#EEE5FF] rounded-2xl flex justify-center items-center">
+                <Icon name="ion:md-wallet" class="text-4xl text-[#7F3DFF]" />
+              </div>
+              <div>
+                <h1 class="text-[#292B2D] text-xl font-medium">
+                  {{ wallet.name }}
+                </h1>
+                <p class="capitalize">{{ wallet.wallet_type.name }}</p>
+              </div>
+            </div>
+            <div class="amount">
+              <p class="text-xl text-[#212325]">${{ wallet.amount }}</p>
+            </div>
           </div>
         </div>
       </div>
@@ -55,13 +61,15 @@
 <script setup>
 const wallet = useWalletType();
 const walletLists = ref([]);
-
+const walletLoading = ref(true);
 const backAction = () => {
   navigateTo('/profile');
 }
 
 const pullWallet = async () => {
+  walletLoading.value = true;
   await wallet.getWalletList();
+  walletLoading.value = false;
   walletLists.value = wallet.walletList;
 };
 pullWallet();
