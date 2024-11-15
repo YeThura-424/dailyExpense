4a85087e1e01d543fd6f54aec07dedf2ea8c0ce1
YeThura-424 <rathanpoudle.26@gmail.com>
2024-11-15 20:08:38 +0630

    add:default modelValue for image preview in FileUpload component

--- components/core/FileUpload.vue
+++ components/core/FileUpload.vue
@@ -1,54 +1,27 @@
 <template>
-  <div
-    :class="[
-      'upload-wrapper border border-[#91919F] h-40 rounded-xl p-3 flex',
-      previewUrl ? 'justify-start gap-3' : 'justify-center items-center',
-    ]"
-  >
+  <div :class="[
+    'upload-wrapper border border-[#91919F] h-40 rounded-xl p-3 flex',
+    previewUrl ? 'justify-start gap-3' : 'justify-center items-center',
+  ]">
     <div class="preview-wrapper" v-if="previewUrl">
-      <div
-        class="preview relative w-24 h-24 border border-dashed border-[#91919F] rounded-xl"
-      >
+      <div class="preview relative w-24 h-24 border border-dashed border-[#91919F] rounded-xl">
         <img :src="previewUrl" alt="" class="w-24 h-24 rounded-xl p-1" />
-        <Icon
-          name="ion:ios-close-circle"
-          class="text-[#666666] absolute top-0 right-0 cursor-pointer"
-          size="24"
-          @click="handleRemove"
-        />
+        <Icon name="ion:ios-close-circle" class="text-[#666666] absolute top-0 right-0 cursor-pointer" size="24"
+          @click="handleRemove" />
       </div>
     </div>
-    <div
-      v-if="previewUrl"
-      class="attachment_input relative w-24 h-24 border border-dashed border-[#91919F] flex flex-col items-center justify-center rounded-xl"
-    >
+    <div v-if="previewUrl"
+      class="attachment_input relative w-24 h-24 border border-dashed border-[#91919F] flex flex-col items-center justify-center rounded-xl">
       <Icon name="ion:ios-plus-outline" size="50" class="text-[#91919F]" />
       <!-- <span class="text-[#91919F] text-lg text-center">Add More</span> -->
-      <input
-        :value="modelValue"
-        id="file-upload"
-        type="file"
-        class="absolute opacity-0 w-full h-full"
-        @change="handleUpload"
-      />
+      <input id="file-upload" type="file" class="absolute opacity-0 w-full h-full" @change="handleUpload" />
     </div>
 
-    <div
-      v-if="!previewUrl"
-      class="attachment_input relative w-32 h-32 border border-dashed border-[#91919F] flex flex-col items-center justify-center rounded-xl"
-    >
-      <Icon
-        name="ion:ios-attach"
-        size="30"
-        class="text-[#91919F] -rotate-[135deg]"
-      />
+    <div v-if="!previewUrl"
+      class="attachment_input relative w-32 h-32 border border-dashed border-[#91919F] flex flex-col items-center justify-center rounded-xl">
+      <Icon name="ion:ios-attach" size="30" class="text-[#91919F] -rotate-[135deg]" />
       <span class="text-[#91919F] text-lg text-center">Add Attachment</span>
-      <input
-        id="file-upload"
-        type="file"
-        class="absolute opacity-0 w-full h-full"
-        @change="handleUpload"
-      />
+      <input id="file-upload" type="file" class="absolute opacity-0 w-full h-full" @change="handleUpload" />
     </div>
   </div>
 </template>
@@ -59,11 +32,15 @@ const props = defineProps({
     type: Boolean,
     default: true,
   },
+  modelValue: {
+    type: String,
+    default: '',
+  }
 });
 const emit = defineEmits(["update:modelValue"]);
 
 const uploadFiles = ref([]);
-const previewUrl = ref(null);
+const previewUrl = ref(props.modelValue);
 const handleUpload = (e) => {
   console.log("uploaded", e.target.files);
   uploadFiles.value = e.target.files;
