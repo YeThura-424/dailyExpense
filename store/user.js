c3cd3cca865949254f3c543e2cad8d59f032cbf9
YeThura-424 <rathanpoudle.26@gmail.com>
2025-01-22 22:02:21 +0630

    update:user store for signup function

--- store/user.js
+++ store/user.js
@@ -1,77 +1,78 @@
 import { defineStore } from "pinia";
 import { supabase } from "../lib/supabaseClient";
 
-export const useUserStore = defineStore(
-  "user",
-  () => {
-    const user = useCookie("user");
-    const token = useCookie("token");
+export const useUserStore = defineStore("user", () => {
+  const user = useCookie("user");
+  const userProfile = useCookie("userProfile");
+  const token = useCookie("token");
 
-    const signup = async (form) => {
-      const { data, error } = await supabase.auth.signUp({
-        email: form.email,
-        password: form.password,
-      });
+  const signup = async (form) => {
+    const { data, error } = await supabase.auth.signUp({
+      email: form.email,
+      password: form.password,
+    });
+    console.log("d nar mar", data);
+    if (error) {
+      return { success: false, error: error.message };
+    }
 
-      if (error) {
-        console.log("register error", error);
-      }
-
-      if (user.data) {
-        const { error } = await supabase.from("profiles").insert({
-          id: data.user.id,
-          username: form.username,
-          currency: form.currency,
-        });
-
-        if (error) {
-          console.log("profile data adding error");
-        }
-      }
-      token.value = data.session; // set token to cookie
-      user.value = data.user;
-      // user.value = data.user;
-      // token.value = data.session;
-      return true;
-    };
-
-    const login = async (formData) => {
-      const { data, error } = await supabase.auth.signInWithPassword({
-        email: formData.email,
-        password: formData.password,
+    if (data.user) {
+      console.log(data, "loggin from profile creation");
+      const { error, data: profile } = await supabase.from("profiles").insert({
+        id: data.user.id,
+        username: form.username,
+        currency: form.currency,
       });
 
       if (error) {
         return { success: false, error: error.message };
       }
 
-      if (data) {
-        user.value = data.user;
-        token.value = data.session;
-        return { success: true };
-      }
+      token.value = data.session; // set token to cookie
+      user.value = data.user;
+      userProfile.value = profile;
 
-      return { success: false, error: "Unknown error occurred." };
-    };
+      return { success: true };
+    }
+    return { success: false, error: "Internal Server Error!!" };
+  };
 
-    const logout = async () => {
-      const { error } = await supabase.auth.signOut();
+  const login = async (formData) => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: formData.email,
+      password: formData.password,
+    });
 
-      if (error) return false;
+    if (error) {
+      return { success: false, error: error.message };
+    }
 
-      user.value = null;
-      token.value = null;
+    if (data) {
+      user.value = data.user;
+      token.value = data.session;
+      return { success: true };
+    }
 
-      return true;
-    };
+    return { success: false, error: "Unknown error occurred." };
+  };
 
-    return {
-      user,
-      token,
-      signup,
-      login,
-      logout,
-    };
-  },
-  { persist: true }
-);
+  const logout = async () => {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) return { success: false, error: error.message };
+
+    user.value = null;
+    token.value = null;
+    userProfile.value = null;
+
+    return true;
+  };
+
+  return {
+    user,
+    token,
+    signup,
+    login,
+    logout,
+  };
+});
