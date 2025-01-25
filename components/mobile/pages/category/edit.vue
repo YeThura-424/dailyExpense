c803c952fe3cc5d1ac09bda93ac1c1843b6ec3be
YeThura-424 <rathanpoudle.26@gmail.com>
2025-01-25 22:49:14 +0630

    category edit page

--- components/mobile/pages/category/edit.vue
+++ components/mobile/pages/category/edit.vue
@@ -3,25 +3,45 @@
     <MobileLoadingDots />
   </div>
   <div class="bg-[#7F3DFF] flex flex-col justify-between h-screen">
-    <div class="expense_main_content px-6 py-4 flex flex-col justify-between h-full">
-      <MobilePageHeader title="Add New Category" icon-color="text-white" @back="backAction" />
+    <div
+      class="expense_main_content px-6 py-4 flex flex-col justify-between h-full"
+    >
+      <MobilePageHeader
+        title="Add New Category"
+        icon-color="text-white"
+        @back="backAction"
+      />
     </div>
-    <div v-if="!categoryDetailLoading" class="expense_form bg-white rounded-t-[40px] shadow px-6 py-4">
+    <div
+      v-if="!categoryDetailLoading"
+      class="expense_form bg-white rounded-t-[40px] shadow px-6 py-4"
+    >
       <div class="account_name">
         <CoreInputBox placeholder="Name" v-model="form.name" />
       </div>
       <div class="category-type py-4">
-        <CoreSelectBox :options="categoryTypes" name="Category" option-key="value" v-model="form.type" />
+        <CoreSelectBox
+          :options="categoryTypes"
+          name="Category"
+          option-key="value"
+          v-model="form.type"
+        />
         <!-- <CoreMultiSelectV2/> -->
       </div>
       <div class="category_attachment py-3">
-        <CoreFileUpload v-model="form.categoryImage" />
+        <CoreFileUpload v-model="form.icon" />
       </div>
 
       <div class="save-button gap-x-5 py-3">
-        <button type="button" @click="updateCategory"
-          class="w-full flex items-center justify-center gap-x-2 rounded-md border border-transparent bg-[#7F3DFF] text-white px-4 py-1.5 text-lg font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 cursor-pointer">
-          <Icon name="ion:save-outline" class="text-white text-2xl cursor-pointer" />
+        <button
+          type="button"
+          @click="updateCategory"
+          class="w-full flex items-center justify-center gap-x-2 rounded-md border border-transparent bg-[#7F3DFF] text-white px-4 py-1.5 text-lg font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 cursor-pointer"
+        >
+          <Icon
+            name="ion:save-outline"
+            class="text-white text-2xl cursor-pointer"
+          />
           Continue
         </button>
       </div>
@@ -30,24 +50,33 @@
 </template>
 
 <script setup>
+import { useCategoryStore } from "~/store/category";
 
 const categoryTypes = ref([
   {
-    name: 'Income',
-    value: 'income'
+    name: "Income",
+    value: "income",
   },
   {
-    name: 'Expend',
-    value: 'expend'
-  }
-])
+    name: "Expend",
+    value: "expend",
+  },
+]);
 const categoryDetailLoading = ref(true);
+const { categoryDetail } = useCategoryStore();
+
 // const type = ref(categoryTypes.value[0].value);
 const form = reactive({
   id: "",
   type: "",
-  categoryImage: "",
+  icon: "",
   name: "",
+});
+
+watch(form.icon, (newval, oldval) => {
+  form.oldIcon = oldval;
+
+  console.log(form);
 });
 
 const route = useRoute();
@@ -56,25 +85,40 @@ const categoryId = route.params.id;
 const loading = ref(false);
 
 const backAction = () => {
-  navigateTo('/category');
-}
+  navigateTo("/category");
+};
 
 const getCategoryDetail = async () => {
   categoryDetailLoading.value = true;
-  await useFetch(`/api/category/${categoryId}`, {
-    method: "GET",
-    transform: (response) => {
-      console.log(response.data)
-      form.name = response?.data?.data?.name;
-      // type.value = categoryTypes.value.find((cate) => cate.value == response.data.type)
-      form.type = (categoryTypes.value.find((cate) => cate.value == response?.data?.data?.type)).value;
-      form.categoryImage = response?.data?.data?.icon;
-      form.id = response?.data?.data?.id;
-    }
-  })
-  categoryDetailLoading.value = false;
+  // await useFetch(`/api/category/${categoryId}`, {
+  //   method: "GET",
+  //   transform: (response) => {
+  //     console.log(response.data);
+  //     form.name = response?.data?.data?.name;
+  //     // type.value = categoryTypes.value.find((cate) => cate.value == response.data.type)
+  //     form.type = categoryTypes.value.find(
+  //       (cate) => cate.value == response?.data?.data?.type
+  //     ).value;
+  //     form.categoryImage = response?.data?.data?.icon;
+  //     form.id = response?.data?.data?.id;
+  //   },
+  // });
+
+  const result = await categoryDetail(categoryId);
+  console.log(categoryId, "right here");
+
+  if (result.success) {
+    // form.name = result.data.name;
+    categoryDetailLoading.value = false;
+  } else {
+    categoryDetailLoading.value = false;
+    useNuxtApp().$toast.error(
+      result.message || "Error Fetching Category Detial..."
+    );
+  }
+
   console.log(form);
-}
+};
 
 const updateCategory = async () => {
   const data = transform(form);
@@ -82,24 +126,24 @@ const updateCategory = async () => {
   try {
     useFetch(`/api/file-upload/category/${form.id}`, {
       method: "POST",
-      body: data
-    })
+      body: data,
+    });
     loading.value = false;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   } finally {
-    navigateTo('/category');
+    navigateTo("/category");
   }
 };
 
 const transform = (form) => {
   let formData = new FormData();
-  formData.append('name', form.name);
-  formData.append('type', form.type);
-  formData.append('updated_icon', form.categoryImage[0]);
+  formData.append("name", form.name);
+  formData.append("type", form.type);
+  formData.append("updated_icon", form.categoryImage[0]);
 
-  return formData
-}
+  return formData;
+};
 
 getCategoryDetail();
 </script>
