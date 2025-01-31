1e87b83810c28d4052da1dede06f48009397b00b
YeThura-424 <rathanpoudle.26@gmail.com>
2025-01-31 22:13:47 +0630

    recent transaction

--- components/core/RecentTransaction.vue
+++ components/core/RecentTransaction.vue
@@ -2,13 +2,18 @@
   <div class="transaction-wrapper">
     <div class="transaction-tab-wrapper">
       <div class="tabs-content flex justify-between space-x-1 rounded-xl p-1">
-        <button v-for="tab in tabs" :key="tab.key" @click="changeTab(tab.key)" :class="[
-          'px-3 rounded-2xl py-2 text-base font-medium leading-5 ',
-          'ring-white/60 ring-offset-2 ring-offset-none focus:outline-none',
-          activeTab == tab.key
-            ? 'bg-[#FCEED4] text-[#FCAC12] shadow'
-            : 'text-[#91919F] hover:bg-white/[0.12] hover:text-white',
-        ]">
+        <button
+          v-for="tab in tabs"
+          :key="tab.key"
+          @click="changeTab(tab.key)"
+          :class="[
+            'px-3 rounded-2xl py-2 text-base font-medium leading-5 ',
+            'ring-white/60 ring-offset-2 ring-offset-none focus:outline-none',
+            activeTab == tab.key
+              ? 'bg-[#FCEED4] text-[#FCAC12] shadow'
+              : 'text-[#91919F] hover:bg-white/[0.12] hover:text-white',
+          ]"
+        >
           {{ tab.name }}
         </button>
       </div>
@@ -17,7 +22,9 @@
       <div class="filter-head flex justify-between items-center pt-2">
         <h1 class="text-xl font-semibold text-[#292B2D]">Filter Transaction</h1>
         <NuxtLink to="/transaction">
-          <button class="text-[#7F3DFF] bg-[#ddd2f1] px-4 py-1.5 rounded-[22px]">
+          <button
+            class="text-[#7F3DFF] bg-[#ddd2f1] px-4 py-1.5 rounded-[22px]"
+          >
             See All
           </button>
         </NuxtLink>
@@ -32,7 +39,9 @@
           <CoreTransactionListCard :transactions="transactions" />
         </div>
         <div v-else class="empty-transactions">
-          <h1 class="text-center py-8">No Transaction for selected criteria!</h1>
+          <h1 class="text-center py-8">
+            No Transaction for selected criteria!
+          </h1>
         </div>
       </div>
     </div>
@@ -40,6 +49,8 @@
 </template>
 
 <script setup>
+import { usetransactionStore } from "~/store/transaction";
+
 const tabs = [
   { name: "Today", key: "today" },
   { name: "Week", key: "week" },
@@ -49,6 +60,7 @@ const tabs = [
 const transactions = ref([]);
 const activeTab = ref(tabs[0].key);
 const transactionLoading = ref(true);
+const transactionStore = usetransactionStore();
 const changeTab = (tab) => {
   activeTab.value = tab;
   fetchTransaction();
@@ -61,21 +73,30 @@ const fetchTransaction = async () => {
     month: activeTab.value === "month",
     year: activeTab.value === "year",
   };
-  try {
-    transactionLoading.value = true;
-    await useFetch("/api/income/list", {
-      method: "GET",
-      params: filterParams,
-      transform: (response) => {
-        transactions.value = response?.data?.data;
-      },
-    });
-  } catch (error) {
-    console.log(error)
-  } finally {
-    transactionLoading.value = false;
-  }
+  // try {
+  //   await useFetch("/api/income/list", {
+  //     method: "GET",
+  //     params: filterParams,
+  //     transform: (response) => {
+  //       transactions.value = response?.data?.data;
+  //     },
+  //   });
+  // } catch (error) {
+  //   console.log(error)
+  // } finally {
+  // }
+  transactionLoading.value = true;
 
+  const result = await transactionStore.fetchTransactionsForToday();
+
+  if (result.success) {
+    console.log(result.data);
+    transactions.value = result.data;
+  } else {
+    transactionLoading.value = false;
+    useNuxtApp().$toast.error(result.error);
+  }
+  transactionLoading.value = false;
 };
 
 fetchTransaction();
