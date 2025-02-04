20ef85eb133763098f6a414a87faafc4abe0ee8d
YeThura-424 <rathanpoudle.26@gmail.com>
2025-02-04 21:58:17 +0630

    budget migration

---
+++ supabase/migrations/20250204152234_budget_schema.sql
@@ -0,0 +1,15 @@
+drop table if exists budget;
+
+create table 
+  budget (
+    id bigint primary key generated always as identity not null,
+    user_id UUID NOT NULL REFERENCES auth.users ON DELETE CASCADE,
+    wallet_id int not null references wallet(id) on delete cascade,
+    total int default 0 not null,
+    spend_amount int null,
+    remaining_amount int null,
+    alert text default false,
+    usage int default 0 null,
+    expired_at timestamptz null,
+    created_at timestamptz default now(),
+  )
