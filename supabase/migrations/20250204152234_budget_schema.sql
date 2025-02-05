5fc59f69df79eb08564f1453c18e2059797589cb
YeThura-424 <rathanpoudle.26@gmail.com>
2025-02-05 21:36:29 +0630

    update:migration

--- supabase/migrations/20250204152234_budget_schema.sql
+++ supabase/migrations/20250204152234_budget_schema.sql
@@ -4,11 +4,12 @@ create table
   budget (
     id bigint primary key generated always as identity not null,
     user_id UUID NOT NULL REFERENCES auth.users ON DELETE CASCADE,
+    category_id bigint not null references categories(id) on delete cascade,
     total int default 0 not null,
     spend_amount int null,
     remaining_amount int null,
     alert text default false,
     usage int default 0 null,
     expired_at timestamptz null,
-    created_at timestamptz default now(),
-  )
+    created_at timestamptz default now()
+  );
