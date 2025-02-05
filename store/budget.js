af2a1c03e737218ea39d71b916a14eb53f289530
YeThura-424 <rathanpoudle.26@gmail.com>
2025-02-05 21:03:44 +0630

    budget store scope

--- store/budget.js
+++ store/budget.js
@@ -8,12 +8,13 @@ export const usebudgetStore = defineStore("budget", () => {
     try {
       const categoryId = payload.categoryId;
       const currentDate = new Date();
+      const currentMonth = currentDate.getMonth();
       const expiredAt = new Date(
         currentDate.getFullYear(),
         date.getMonth() + 1,
         0
       );
-      const { data: budgetData, error: budgetError } = supabase
+      const { data: budgetData, error: budgetError } = await supabase
         .from("budget")
         .select()
         .eq("category_id", categoryId)
@@ -21,8 +22,53 @@ export const usebudgetStore = defineStore("budget", () => {
         .single();
 
       if (budgetError) throw new Error(budgetError.message);
+
+      if (budgetData) {
+        const budgetExpireAt = new Date(budgetData.expired_at).getMonth();
+        if (budgetExpireAt == currentMonth) {
+          const { budgetUpdateError } = await supabase
+            .from("budget")
+            .update({
+              total: payload.total,
+              alert: payload.alert,
+              category_id: categoryId,
+            })
+            .eq("id", budgetData.id);
+
+          if (budgetUpdateError) throw new Error(budgetUpdateError.message);
+        } else {
+          const result = await storeBudget(payload, expiredAt);
+
+          if (!result) throw new Error(result.error);
+        }
+      } else {
+        const result = await storeBudget(payload, expiredAt);
+
+        if (!result) throw new Error(result.error);
+      }
+
+      return { success: true };
     } catch (error) {
       return { success: false, error: error.message };
     }
+  };
+
+  const storeBudget = async (payload, expiredAt) => {
+    const { data, error } = await supabase.from("budget").insert({
+      user_id: user.value.id,
+      total: payload.total,
+      alert: payload.alert,
+      expired_at: expiredAt,
+    });
+
+    if (error) {
+      return { success: false, error: error.message };
+    }
+
+    return { success: true, data: data };
+  };
+
+  return {
+    createBudget,
   };
 });
--- supabase/migrations/20250204152234_budget_schema.sql
+++ supabase/migrations/20250204152234_budget_schema.sql
@@ -4,7 +4,6 @@ create table
   budget (
     id bigint primary key generated always as identity not null,
     user_id UUID NOT NULL REFERENCES auth.users ON DELETE CASCADE,
-    wallet_id int not null references wallet(id) on delete cascade,
     total int default 0 not null,
     spend_amount int null,
     remaining_amount int null,
