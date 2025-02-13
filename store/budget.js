e13fd8ee428408567dc1ee08a282fff63cb9e9db
YeThura-424 <rathanpoudle.26@gmail.com>
2025-02-13 22:20:34 +0630

    multiple category in budget

--- store/budget.js
+++ store/budget.js
@@ -94,8 +94,77 @@ export const useBudgetStore = defineStore("budget", () => {
     return { success: true, data: data };
   };
 
+  const createBudgetV2 = async (payload) => {
+    try {
+      const categoryId = payload.categoryId;
+      const currentDate = new Date();
+      const currentMonth = currentDate.getMonth();
+      const expiredAt = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth() + 1,
+        0
+      );
+      const { data: budgetData } = await supabase
+        .from("budget")
+        .select("*, budget_categories(*)")
+        .in("budget_categories.category_id", categoryId)
+        .eq("user_id", user.value.id)
+        .gte("expired_at", currentDate.toISOString())
+        .single();
+
+      if (budgetData) {
+        const budgetExpireAt = new Date(budgetData.expired_at).getMonth();
+        if (budgetExpireAt == currentMonth) {
+          const updateRemaining = payload.total + budgetData.remaining_amount;
+          const { budgetUpdateError } = await supabase
+            .from("budget")
+            .update({
+              total: payload.total,
+              alert: payload.alert,
+              remaining_amount: updateRemaining,
+              // category_id: categoryId,
+            })
+            .eq("id", budgetData.id);
+
+          if (budgetUpdateError) throw new Error(budgetUpdateError.message);
+        } else {
+          const result = await storeBudget(payload, expiredAt);
+
+          if (!result.success) throw new Error(result.error);
+        }
+      } else {
+        const result = await storeBudget(payload, expiredAt);
+
+        if (!result.success) throw new Error(result.error);
+      }
+
+      return { success: true };
+    } catch (error) {
+      return { success: false, error: error.message };
+    }
+  };
+
+  const storeBudgetV2 = async (payload, expiredAt) => {
+    const { data, error } = await supabase.from("budget").insert({
+      user_id: user.value.id,
+      // category_id: payload.categoryId,
+      total: payload.total,
+      remaining_amount: payload.total,
+      alert: payload.alert,
+      expired_at: expiredAt,
+    });
+
+    if (error) {
+      return { success: false, error: error.message };
+    }
+
+    return { success: true, data: data };
+  };
+
   return {
     createBudget,
     fetchBudget,
+    createBudgetV2,
+    storeBudgetV2,
   };
 });
