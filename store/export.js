0283a4c51d4ca211a60a918f795a8a26526dd214
YeThura-424 <rathanpoudle.26@gmail.com>
2025-02-24 22:16:44 +0630

    budget export api

--- store/export.js
+++ store/export.js
@@ -91,8 +91,71 @@ export const useExportStore = defineStore("export", () => {
     }
   };
 
+  const exportBudget = async (payload) => {
+    try {
+      let query = supabase..from("budget")
+        .select(
+          `
+          *,
+          budget_categories(*, categories(name),budget(*))
+        `
+        );
+
+      if (payload.range == "day" && payload.from_date && payload.to_date) {
+        const fromDate = new Date(payload.from_date)
+          .toISOString()
+          .split("T")[0];
+
+        query = query.filter("created_at", "gte", fromDate);
+
+        const toDate = new Date(payload.to_date).toISOString().split("T")[0];
+
+        query = query.filter("created_at", "lte", toDate);
+      }
+      if (payload.range == "month" && payload?.month && payload?.year) {
+        query = query.filter(
+          "created_at",
+          "gte",
+          `${payload?.year}-${payload?.month.toString().padStart(2, "0")}-01`
+        );
+        query = query.filter(
+          "created_at",
+          "lt",
+          `${payload?.year}-${(payload?.month + 1)
+            .toString()
+            .padStart(2, "0")}-01`
+        );
+      }
+
+      if (payload.range == "year" && payload?.year) {
+        query = query
+          .gte("created_at", `${payload?.year}-01-01`)
+          .lte("created_at", `${payload?.year}-12-31`);
+      }
+
+      const { data, error } = await query
+        .eq("user_id", authUser.value.id)
+        .order("created_at", {
+          ascending: false,
+        });
+
+      if (error) throw new Error(error.message);
+
+      return {
+        success: true,
+        data: data,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        error: error.message,
+      };
+    }
+  }
+
   return {
     exportTransaction,
     exportWallet,
+    exportBudget
   };
 });
