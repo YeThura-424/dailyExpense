ceb056f7e83069a3dc65f2a93492f7394e275fc9
YeThura-424 <rathanpoudle.26@gmail.com>
2025-03-21 22:01:41 +0630

    expense update component

--- components/mobile/pages/expenseDetail.vue
+++ components/mobile/pages/expenseDetail.vue
@@ -2,11 +2,8 @@
   <div class="main-wrapper" v-if="expenseData">
     <div class="bg-[#FD3C4A] p-3 rounded-b-[40px] relative">
       <div class="main-header">
-        <MobilePageHeader
-          title="Expense Detail"
-          icon-color="text-white"
-          @back="backAction"
-        />
+        <!-- show-delete @delete="deleteAction" -->
+        <MobilePageHeader title="Expense Detail" icon-color="text-white" @back="backAction" />
       </div>
       <div class="main-body text-center text-white pt-20 pb-8">
         <h1 class="amount font-extrabold text-5xl py-2">
@@ -17,8 +14,7 @@
         </p>
       </div>
       <div
-        class="expense-type-wrapper shadow-md bg-white flex items-center gap-x-4 rounded-lg py-4 px-6 absolute left-1/2 -translate-x-1/2 w-[90%] -bottom-[73px]"
-      >
+        class="expense-type-wrapper shadow-md bg-white flex items-center gap-x-4 rounded-lg py-4 px-6 absolute left-1/2 -translate-x-1/2 w-[90%] -bottom-[73px]">
         <div class="type text-end w-1/3">
           <p class="text-lg text-[#91919F]">Type :</p>
           <p class="text-lg text-[#91919F]">Category :</p>
@@ -53,18 +49,13 @@
         <img src="/images/receipt.png" alt="" />
       </div>
 
-      <!-- <div class="pb-4">
-        <button
-          type="button"
-          class="w-full flex items-center gap-x-2 justify-center rounded-md border border-transparent bg-[#7F3DFF] text-white px-4 py-1.5 text-lg font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 cursor-pointer"
-        >
-          <Icon
-            name="ion:arrow-up-left-box-outline"
-            class="text-white text-2xl cursor-pointer"
-          />
+      <div class="pb-4">
+        <button type="button" @click="editExpense"
+          class="w-full flex items-center gap-x-2 justify-center rounded-md border border-transparent bg-[#7F3DFF] text-white px-4 py-1.5 text-lg font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 cursor-pointer">
+          <Icon name="ion:arrow-up-left-box-outline" class="text-white text-2xl cursor-pointer" />
           Edit
         </button>
-      </div> -->
+      </div>
     </div>
   </div>
 </template>
@@ -94,6 +85,10 @@ const fetchExpense = async () => {
     router.back();
   }
 };
+
+const editExpense = () => {
+  navigateTo(`/expense/update/${expenseId}`);
+}
 
 fetchExpense();
 </script>
