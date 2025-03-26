df1504ec1c121a9f12340e2c5b823795c1b99f90
YeThura-424 <rathanpoudle.26@gmail.com>
2025-03-26 21:52:53 +0630

    update transaction list card

--- components/core/TransactionListCard.vue
+++ components/core/TransactionListCard.vue
@@ -1,26 +1,15 @@
 <template>
   <div v-for="transaction in transactions" :key="transaction.id">
-    <nuxt-link
-      :to="`/${transaction.type}/${transaction.id}`"
-      class="flex justify-between items-center bg-[#FCFCFC] p-4 rounded-lg mt-2"
-    >
+    <nuxt-link :to="`/${transaction.type}/${transaction.id}`"
+      class="flex justify-between items-center bg-[#FCFCFC] p-4 rounded-lg mt-2">
       <div class="transaction_type w-3/4 flex gap-x-4">
-        <div
-          :class="[
-            'transaction_list_icon w-14 h-14 flex justify-center items-center rounded-xl',
-            transaction.type == 'expense' ? 'bg-[#FDD5D7]' : 'bg-[#CFFAEA] ',
-          ]"
-        >
-          <img
-            v-if="transaction?.category?.icon"
-            :src="transaction.category.icon"
-            class="w-full h-full rounded-xl"
-          />
+        <div :class="[
+          'transaction_list_icon w-14 h-14 flex justify-center items-center rounded-xl',
+          transaction.type == 'expense' ? 'bg-[#FDD5D7]' : 'bg-[#CFFAEA] ',
+        ]">
+          <img v-if="transaction?.category?.icon" :src="transaction.category.icon" class="w-full h-full rounded-xl" />
           <div v-else class="default-icon">
-            <IconHomeExpense
-              v-if="transaction.type == 'expense'"
-              fill="#FDD5D7"
-            />
+            <IconHomeExpense v-if="transaction.type == 'expense'" fill="#FDD5D7" />
             <IconHomeIncome v-else fill="#CFFAEA" />
           </div>
         </div>
@@ -37,13 +26,10 @@
         <span class="text-sm text-[#91919F] truncate">{{
           transaction.wallet.name
         }}</span>
-        <h1
-          v-if="transaction.type == 'expense'"
-          class="font-semibold text-lg text-[#FD3C4A]"
-        >
+        <h1 v-if="transaction.type == 'expense'" class="font-semibold text-lg text-[#FD3C4A] truncate">
           {{ getCurrency() }}-{{ transaction.amount }}
         </h1>
-        <h1 v-else class="font-semibold text-lg text-[#00A86B]">
+        <h1 v-else class="font-semibold text-lg text-[#00A86B] truncate">
           {{ getCurrency() }}+{{ transaction.amount }}
         </h1>
       </div>
