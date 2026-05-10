82eae7b7d45319727d88216daec381681f724ecf
YeThura-424 <68313967+YeThura-424@users.noreply.github.com>
2026-05-10 20:55:26 +0400

    update: signup page

--- components/mobile/pages/auth/signup.vue
+++ components/mobile/pages/auth/signup.vue
@@ -1,25 +1,23 @@
 <template>
+  <MobileLoadingDots v-if="loading" />
   <div class="login-wrapper px-6 py-4">
     <MobilePageHeader title="Sign Up" text-color="text-black" :show-back="false" />
     <div class="login-form-wrapper pt-10">
       <div class="name py-3">
-        <CoreInputBox v-model="form.name" placeholder="Name" type="text" />
-        <p v-if="validationError?.name" class="text-sm text-red-600 font-normal">{{ validationError.name[0] }}</p>
+        <CoreInputBox v-model="form.username" placeholder="Name" type="text" />
       </div>
 
       <div class="currency py-3">
-        <CoreSelectBox placeholder="Select Default Currency" :options="currency" show-icon />
+        <CoreSelectBox placeholder="Select Default Currency" :options="currency" v-model="form.currency"
+          option-key="value" show-icon />
       </div>
 
       <div class="email py-3">
         <CoreInputBox v-model="form.email" placeholder="Email" type="email" />
-        <p v-if="validationError?.email" class="text-sm text-red-600 font-normal">{{ validationError.email[0] }}</p>
       </div>
 
       <div class="password py-3">
         <CoreInputBox v-model="form.password" placeholder="Password" type="password" />
-        <p v-if="validationError?.password" class="text-sm text-red-600 font-normal">{{ validationError.password[0] }}
-        </p>
       </div>
 
       <div class="password py-3">
@@ -27,15 +25,16 @@
       </div>
 
       <div class="checkbox flex gap-x-2">
-        <input id="termsandcondition" type="checkbox"
-          class="h-8 w-8 border-2 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" />
+        <input id="termsandcondition" type="checkbox" v-model="form.terms"
+          class="shrink-0 h-7 w-7 border-2 rounded cursor-pointer accent-[#7F3DFF] border-gray-300" />
         <label for="termsandcondition" class="text-lg">By signing up, you agree to the
           <span class="text-[#7F3DFF]">Terms of Service and Privacy Policy</span>
         </label>
       </div>
+
       <div class="sign-up py-3">
-        <button type="button" @click="signup"
-          class="w-full flex items-center justify-center gap-x-2 rounded-md border border-transparent bg-[#7F3DFF] text-white px-4 py-1.5 text-lg font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 cursor-pointer">
+        <button type="button" @click="registerUser" :disabled="!isReadySignUp ? true : false"
+          class="w-full flex items-center justify-center gap-x-2 rounded-md border border-transparent bg-[#7F3DFF] text-white px-4 py-1.5 text-lg font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 cursor-pointer disabled:opacity-80 disabled:cursor-not-allowed">
           <Icon name="ion:ios-log-in" class="text-white text-2xl cursor-pointer" />
           Sign Up
         </button>
@@ -54,37 +53,54 @@
 </template>
 
 <script setup>
+import { useUserStore } from "~/store/user";
+const loading = ref(false);
+const isReadySignUp = ref(false);
 
 const currency = ref([
-  { name: 'Ks', value: 'ks', icon: '/images/ks.png' },
-  { name: 'USD', value: 'usd', icon: '/images/dollar.png' },
-  { name: 'Baht', value: 'bhat', icon: '/images/baht.png' },
-])
+  { name: "Ks", value: "ks", icon: "/images/ks.png" },
+  { name: "USD", value: "usd", icon: "/images/dollar.png" },
+  { name: "Baht", value: "baht", icon: "/images/baht.png" },
+  { name: "AED", value: "aed", icon: "/images/aed.png" },
+]);
+
+const router = useRouter();
 const form = reactive({
-  name: '',
-  email: '',
-  password: '',
-  password_confirmation: '',
-  currency: '',
-})
+  username: "",
+  email: "",
+  password: "",
+  password_confirmation: "",
+  currency: "",
+  terms: false,
+});
 
-const validationError = ref(null)
-const token = useCookie("token");
-const user = useCookie("user");
+const { signup } = useUserStore();
 
-const signup = async () => {
-  const { data, status, error } = await useFetch("/api/register", {
-    method: 'POST',
-    body: form,
-  });
-  if (status.value == 'success') {
-    token.value = data?.value?.data?.token; // set token to cookie
-    user.value = data?.value?.data?.user;
-    navigateTo('/');
+watch(form, (newValue) => {
+  if (newValue.password == newValue.password_confirmation && newValue.terms) {
+    isReadySignUp.value = true;
+  } else {
+    isReadySignUp.value = false;
   }
+});
 
-  if (error.value) {
-    validationError.value = error.value?.data?.data?.errors;
+const registerUser = async (e) => {
+  loading.value = true;
+
+  if (e.isTrusted) {
+    // to ensure that the click event is from user
+    const result = await signup(form);
+
+    if (result.success) {
+      useNuxtApp().$toast.success("Register Successful");
+      router.push("/");
+    } else {
+      useNuxtApp().$toast.error(
+        result.error || "Register failed. Please try again."
+      );
+    }
+
+    loading.value = false;
   }
-}
+};
 </script>
